100439afa38c59a520629f70e2b4e72bf2bad7cc
BIT Student <classroom@bgit.rs>
2020-06-18 17:19:42 +0200

    My Team component added to the main page

--- src/components/Main/TeamMember/TeamMember.js
+++ src/components/Main/TeamMember/TeamMember.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import style from './TeamMember.module.css'
+
 export const TeamMember = (props) => {
     return (
-        <li key={props.id}>
-            <img src={props.url} alt='Oops...'></img>
+        <li className={style.wrapper}>
+            <img src={props.url} alt='Oops...' className={style.member}></img>
+            <img src="https://cdn.iconscout.com/icon/free/png-512/delete-737-475058.png" alt="delete" className={style.deleteIcon} />
             <p>{props.name}</p>
         </li>
     )
